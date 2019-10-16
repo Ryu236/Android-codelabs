5391859acec1793f58df11f43e1cb62f049a52f3
Ryu236 <ryu.adv.2360@gmail.com>
2019-10-16 23:51:08 +0900

    :sparkles: Feat: coding step6 SavedStateVMFactory

--- android-lifecycles/app/src/main/java/com/example/android/lifecycles/step6/SavedStateActivity.java
+++ android-lifecycles/app/src/main/java/com/example/android/lifecycles/step6/SavedStateActivity.java
@@ -22,11 +22,12 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.android.codelabs.lifecycle.R;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.SavedStateVMFactory;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.example.android.codelabs.lifecycle.R;
 
 /**
  * Shows a simple form with a button and displays the value of a property in a ViewModel.
@@ -42,13 +43,13 @@ public class SavedStateActivity extends AppCompatActivity {
 
         // TODO: Pass a SavedStateVMFactory so you can use SavedStateHandle
         // Obtain ViewModel
-        mSavedStateViewModel = ViewModelProviders.of(this).get(SavedStateViewModel.class);
+        mSavedStateViewModel = ViewModelProviders.of(this, new SavedStateVMFactory(this)).get(SavedStateViewModel.class);
 
         // Show the ViewModel property's value in a TextView
         mSavedStateViewModel.getName().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String savedString) {
-                ((TextView)findViewById(R.id.saved_vm_tv))
+                ((TextView) findViewById(R.id.saved_vm_tv))
                         .setText(getString(R.string.saved_in_vm, savedString));
 
             }
@@ -58,7 +59,7 @@ public class SavedStateActivity extends AppCompatActivity {
         findViewById(R.id.save_bt).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newName = ((EditText)findViewById(R.id.name_et)).getText().toString();
+                String newName = ((EditText) findViewById(R.id.name_et)).getText().toString();
                 mSavedStateViewModel.saveNewName(newName);
             }
         });
