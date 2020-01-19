1adbc5f3dd5acba97e4db540b3a17699031137ea
Ryu236 <ryu.adv.2360@gmail.com>
2020-01-19 23:31:57 +0900

    :recycle: Reafactor(activity): change name of activity main → tracking

--- TransportTracker/app/src/main/java/com/ryu236/transporttracker/TrackerActivity.kt
+++ TransportTracker/app/src/main/java/com/ryu236/transporttracker/TrackerActivity.kt
@@ -3,7 +3,7 @@ package com.ryu236.transporttracker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class TrackerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
