5fd90b3430e05d7265805f186d12e715f3f26ab5
Ryu236 <ryu.adv.2360@gmail.com>
2020-01-22 23:12:59 +0900

    :sparkles: Feat(service): add tracker service

--- TransportTracker/app/src/main/java/com/ryu236/transporttracker/TrackerActivity.kt
+++ TransportTracker/app/src/main/java/com/ryu236/transporttracker/TrackerActivity.kt
@@ -1,12 +1,62 @@
 package com.ryu236.transporttracker
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class TrackerActivity : AppCompatActivity() {
+class TrackerActivity : Activity() {
+
+    private val PERMISSIONS_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check GPS is enabled
+        val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE)
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        // Check location permission is granted = if it is, start the service,
+        // otherwise request the permission
+        val permission: Int =
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService()
+        } else {
+            ActivityCompat.requestPermissions(this, String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION
+            }, PERMISSIONS_REQUEST)
+        }
+    }
+
+    private fun startTrackerService() {
+        startService(Intent(this, TrackerService::class.java))
+        finish()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Start the service when the permission is granted
+            startTrackerService()
+        } else {
+            finish()
+        }
     }
 }
